bd459a458d5e241642aea62c0971f3818b673a12
MerowingerX <frank.simon.bs@gmail.com>
2026-03-02 19:33:43 +0000

    description of start

--- start_server.sh
+++ start_server.sh
@@ -1,1 +1,29 @@
+#! /bin/bash
+# use this to start the application
 .venv/bin/uvicorn web.main:app --host 0.0.0.0 --port 1946 --reload
+
+# or this to start the service
+#
+# file with content:
+# /etc/systemd/system/sportplatz-homepage-uvicorn.service
+# [Unit]
+# Description=Sportplatz Homepage (Uvicorn)
+# After=network.target
+#
+# [Service]
+# Type=simple
+# User=tus # oder dein Nutzer
+# Group=tus
+# WorkingDirectory=/home/tus/sportplatz-homepage   # dein Projektordner
+# ExecStart=/home/tus/sportplatz-homepage/.venv/bin/uvicorn web.main:app --host 0.0.0.0 --port 1946 --reload
+#
+# Restart=always
+# RestartSec=3
+# Environment=PYTHONUNBUFFERED=1
+#
+# [Install]
+# WantedBy=multi-user.target
+
+# and the command to start or restart the service:
+# sudo systemctl restart sportplatz-buchung.service
+# sudo systemctl start sportplatz-buchung.service
